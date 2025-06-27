d223e5a75a9b27c4500feb594ac1d9a072d88dd8
mengyunwu <60383029+mengyunwu2020@users.noreply.github.com>
2025-06-26 21:32:33 -0400

    Update superviseNet.R

--- R/superviseNet.R
+++ R/superviseNet.R
@@ -448,3 +448,20 @@ superviseNet<-function(
 
 
 }
+
+
+               
+
+Gamma_int<-function(v_0, v_1, p_2, p, k,Theta_l){
+
+  P_l=list();
+  for(i in 1:k) {
+    P_l[[i]] = (1.0/(1.0 + v_1/v_0*exp(-abs((Theta_l[[i]]))/v_0 + abs((Theta_l[[i]]))/v_1)*(1-p_2)/p_2));
+  }
+
+
+  return(P_l)
+
+}
+
+
